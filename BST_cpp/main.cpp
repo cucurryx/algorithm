d6f93466c02467dd0da09532227648f9ad66aa04
xiebei1108 <xiebei1108@outlook.com>
2017-07-25 20:45:23 +0000

    Binary search tree in c++

--- BST_cpp/main.cpp
+++ BST_cpp/main.cpp
@@ -4,17 +4,21 @@ int main()
 {
 	BST<int, int> tree;
 
-	for(int i = 0; i < 100; ++i)
+	for(int i = 0; i != 100; ++i)
 		tree.put(i, i);
 	tree.deleteMin();
 	tree.deleteMax();
 	cout<<tree.min()<<" "<<tree.max()<<endl;
 	cout<<tree.size()<<endl;
 
-	for(int i = 1; i < 90; ++i)
+	for(int i = 1; i != 90; ++i)
 		tree.erase(i);
 	cout<<tree.min()<<" "<<tree.max()<<endl;
 	cout<<tree.size()<<endl;
+
+	for(int i = 90; i != 100; ++i)
+		if(tree.get(i) != static_cast<int>(NULL))
+			cout<<tree.get(i)<<endl;
 
 	return 0;
 }
